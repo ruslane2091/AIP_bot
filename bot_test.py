270581d3baae6ec2f6d4ab1633e19886d2756ba3
ruslane2091 <116846667+ruslane2091@users.noreply.github.com>
2022-12-22 19:26:04 +0300

    Add files via upload

---
+++ bot_test.py
@@ -0,0 +1,26 @@
+import unittest
+from CREED_bot import check, check_adm, number
+
+
+class messageTest(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(check('инцел'), 1)
+
+    def test_2(self):
+        self.assertEqual(check('воробушек'), 0)
+
+    def test_3(self):
+        self.assertEqual(check('Не Гр'), 1)
+
+    def test_bd(self):
+        self.assertEqual(int(check_adm('ruslane2091#6184')), 1)
+
+    def test_bd2(self):
+        self.assertEqual(int(check_adm('User1234#1111')), 0)
+
+    def test_emoji(self):
+        self.assertEqual(str(number('👍')), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
